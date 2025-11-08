d2d63d936ee4f6cc614b51303070d77b6dd277f7
Matthew Heffernan <mrhheffernan@gmail.com>
2025-11-08 15:08:51 -0800

    Clarify variable names

--- src/lib.rs
+++ src/lib.rs
@@ -106,9 +106,9 @@ pub mod lhd {
         for dimension in 0..n_dim {
             // n_samples in an LHD is the same as the number of intervals to fill
             let mut counts = vec![0; n_samples];
-            for interval in 0..n_samples {
+            for sample_idx in 0..n_samples {
                 // back-engineer the interval
-                let sample = design[interval][dimension];
+                let sample = design[sample_idx][dimension];
                 let sample_interval_idx = (sample * n_samples as f64).floor() as usize;
                 counts[sample_interval_idx] += 1;
             }
