39c144c5017dddfb514bb72a7629570f5fab8109
Matthew Heffernan <mrhheffernan@gmail.com>
2026-02-09 21:35:14 -0800

    Move python bindings over, clean up old entrypoints

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,16 @@ pub mod lhd;
 pub mod maximin_utils;
 pub mod maxpro_utils;
 
+/// Using many iterations, select a LHD that optimizes an allowed metric.
+///
+/// Arguments:
+///     n_samples (u64): Number of samples
+///     n_dim (u64): Number of dimensions
+///     n_iterations (u64): Number of iterations
+///     metric (Some(enums::Metrics)): Metric to consider
+///
+/// Returns:
+///     Vec<Vec<f64>>: Latin hypercube design that optimizes a metric using random sampling
 pub fn build_lhd(
     n_samples: u64,
     n_dim: u64,
@@ -69,6 +79,55 @@ pub fn build_lhd(
     best_lhd_metric_pair.0 // only return the lhd, not the metric
 }
 
+#[cfg(feature = "pyo3-bindings")]
+/// Build an optimal LHD
+///
+/// Args:
+///     n_samples (int): Number of samples for the LHD
+///     n_dim (int): Number of dimensions in which to generate points
+///     n_iterations (int): Number of iterations to use to search for an optimal LHD
+///     metric (str): Metric to use. Must be either 'maximin' or 'maxpro'.
+///
+/// Returns:
+///     list[list[float]]: A semi-optimal maximin latin hypercube design
+#[pyfunction(name = "build_maximin_lhd")]
+pub fn py_build_lhd(
+    n_samples: u64,
+    n_dim: u64,
+    n_iterations: u64,
+    metric: str,
+) -> PyResult<Vec<Vec<f64>>> {
+    if n_samples == 0 {
+        return Err(PyValueError::new_err(
+            "n_samples must be positive and nonzero",
+        ));
+    }
+    if n_dim == 0 {
+        return Err(PyValueError::new_err("n_dim must be positive and nonzero"));
+    }
+    if n_iterations == 0 {
+        return Err(PyValueError::new_err(
+            "n_iterations must be positive and nonzero",
+        ));
+    }
+    if n_samples > usize::MAX as u64 {
+        return Err(PyValueError::new_err("n_samples too large to index"));
+    }
+    if n_dim > usize::MAX as u64 {
+        return Err(PyValueError::new_err("n_dim too large to index"));
+    }
+    if metric == "maxpro" {
+        let pymetric = enums::Metrics::MaxPro;
+    } else if metric == "maximin" {
+        let pymetric = enums::Metrics::MaxiMin;
+    } else {
+        return Err(PyValueError::new_err(
+            "Metric must be 'maxpro' or 'maximin'.",
+        ));
+    }
+    Ok(build_lhd(n_samples, n_dim, n_iterations, pymetric))
+}
+
 // Python module definition
 #[cfg(feature = "pyo3-bindings")]
 #[pymodule]
@@ -76,8 +135,7 @@ fn maxpro(_py: Python<'_>, m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(lhd::py_generate_lhd, m)?)?;
     m.add_function(wrap_pyfunction!(maxpro_utils::py_build_maxpro_lhd, m)?)?;
     m.add_function(wrap_pyfunction!(maxpro_utils::py_maxpro_criterion, m)?)?;
-    m.add_function(wrap_pyfunction!(maximin_utils::py_build_maximin_lhd, m)?)?;
-    m.add_function(wrap_pyfunction!(maximin_utils::py_maximin_criterion, m)?)?;
+    m.add_function(wrap_pyfunction!(py_build_lhd, m)?)?;
     m.add_function(wrap_pyfunction!(anneal::py_anneal_lhd, m)?)?;
     Ok(())
 }
